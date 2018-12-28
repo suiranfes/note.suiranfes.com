c9d7339f1d4721c86a06cd1555a4354c80a41fde
tamaina <tamaina@hotmail.co.jp>
2018-12-28 16:03:27 +0900

    fix grid

--- pushAll.bat
+++ pushAll.bat
@@ -1,13 +1,16 @@
 git fetch
+
+echo 'リモートと競合した場合はpullAll.batを同様に実行してみてください。'
+
+git add --all
+git commit -a -m %1
+git push -u origin src
 cd pages
 git add --all
 git commit -a -m %1
-git push
+git push -u origin pages
 cd ../docs
 git add --all
 git commit -a -m %1
-git push
+git push -u origin master
 cd ..
-git add --all
-git commit -a -m %1
-git push
